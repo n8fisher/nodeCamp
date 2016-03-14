ab88c6b897c1acc9735d69246ed7c045dc3d6eeb
Fisher <fisher.nathaniel@gmail.com>
2016-03-13 21:40:37 -0700

    Tested

--- prework/index.js
+++ prework/index.js
@@ -2,22 +2,47 @@
 
 let http = require('http');
 let request = require('request');
-let destinationUrl = '127.0.0.1:8000';
+let argv = require('yargs')
+    .default('host', '127.0.0.1')
+    .argv;
+let scheme = 'http://';
+let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80);
+let destinationUrl = scheme  + argv.host + ':' + port;
+
+let path = require('path');
+let fs = require('fs');
+let logPath = argv.log && path.join(__dirname, argv.log);
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout;
+console.log(argv.log)
 
 http.createServer((req, res) => {
 	console.log(`Request received at: ${req.url}`)
-	res.end('hello world\n')
+	for (let header in req.headers) {
+	    res.setHeader(header, req.headers[header])
+	    logStream.write('Request headers: ' + JSON.stringify(req.headers))
+	}
+	req.pipe(res)
     }).listen(8000);
 
 http.createServer((req, res) => {
-	// Proxy code here
-	console.log(`Proxying request to: ${destinationUrl + req.url}`)
-	let options = {
-	    headers: req.headers,
-	    url: `http://${destinationUrl}${req.url}`
-	};
-	options.method = req.method
-	req.pipe(request(options)).pipe(res)
+	try{
+	    console.log(`Proxying request to: ${destinationUrl + req.url}`)
+	    let options = {
+		headers: req.headers,
+		url: `${destinationUrl}${req.url}`
+	    };
+
+	    options.method = req.method
+	    let downstreamResponse = req.pipe(request(options))
+	    logStream.write('Request headers: ' + JSON.stringify(req.headers))
+	    logStream.write('Request headers: ' + JSON.stringify(downstreamResponse.headers))
+	    downstreamResponse.pipe(process.stdout)
+	    downstreamResponse.pipe(res)
+
+	}catch(err){
+	    process.stdout.write(err)
+	}
+
 
     }).listen(8001);
 
