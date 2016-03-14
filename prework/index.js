7a436fcbdce5b1ecc2ec5a632db22afa182216c8
Nathaniel Fisher <n3fishe@XXXWMXXX.local>
2016-03-13 19:34:09 -0700

    Working proxy code

--- prework/index.js
+++ prework/index.js
@@ -1,7 +1,24 @@
 "use strict";
-let http = require('http')
+
+let http = require('http');
+let request = require('request');
+let destinationUrl = '127.0.0.1:8000';
 
 http.createServer((req, res) => {
 	console.log(`Request received at: ${req.url}`)
 	res.end('hello world\n')
-}).listen(8000)
+    }).listen(8000);
+
+http.createServer((req, res) => {
+	// Proxy code here
+	console.log(`Proxying request to: ${destinationUrl + req.url}`)
+	let options = {
+	    headers: req.headers,
+	    url: `http://${destinationUrl}${req.url}`
+	};
+	options.method = req.method
+	req.pipe(request(options)).pipe(res)
+
+    }).listen(8001);
+
+
